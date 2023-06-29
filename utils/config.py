b96292a7ccdd8fc8cdd7a77be5bc2a64d4f18d43
wangsssky <wanglin.mailbox@gmail.com>
2023-06-29 14:26:02 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -42,4 +42,7 @@ class Config_SAM:
         self.VISUALIZE = yaml_dict['VISUALIZE']
         self.EVAL_METRIC = yaml_dict['EVAL_METRIC']
 
+        if self.SAM_NAME == 'mobile':
+            self.IS_FINETUNE_IMAGE_ENCODER = False
+
 
